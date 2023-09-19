25ace2b3bcdf203fba7e1305efc3632e4ac5ef87
Alex Henrique <alex00henrique@gmail.com>
2023-09-19 15:58:39 -0300

    Create 3_operadores_logicos.dart

---
+++ dart_fundamentos/lib/3_operadores/3_operadores_logicos.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: unused_local_variable
+
+void main() {
+  // operadores logicos
+
+  // &&(E)
+
+  final sexo = 'M';
+  final idade = 18;
+
+  // if (sexo == 'M') {
+  //   if (idade >= 18) {
+  //     print('Pode entrar');
+  //   } else {
+  //     print('Não pode entrar');
+  //   }
+  // } else {
+  //   print('Não pode entrar');
+  // }
+
+  if (sexo == 'M' && idade >= 18) {
+    print('Pode entrar');
+  } else {
+    print('Não pode entrar');
+  }
+}
